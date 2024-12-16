7ab3ab5d7daec1910b3163689ecde85eda3dcfbc
whishasha <wilsonma12746@gmail.com>
2024-12-16 15:21:38 +1100

    reviews now writable to database
    reviews placed within movies page writable to database if logged in

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ movieReview = cur.execute('SELECT * FROM reviews').fetchall()
 
 movieUsername = cur.execute('SELECT name FROM user').fetchall()
 
+
 cur.close()
 con.close()
 
@@ -90,13 +91,13 @@ def login_1():
             if exists == True:
                 global login
                 login = True # user is now logged in, allowing pages to have their logged in version
-                return f"Account exists, with name {name}"
+                return redirect("/")
                 
             else:
                 return "You DOG."
         return render_template('login1.html')
 
-@app.route('/users/<string:name>')
+@app.route('/users/<string:name>', methods = ['GET', 'POST'])
 def user(name):
     userReview = []
     for review in movieReview:
@@ -111,12 +112,62 @@ def user(name):
     return 'Error! User not found!' #replace with error page
 
 #Dynamically creates movie pages for each movie in the SWCG database.
-@app.route('/movies/<int:movieNumber>') #To each specific movie
+@app.route('/movies/<int:movieNumber>', methods = ['GET', 'POST']) #To each specific movie
 def movie(movieNumber): #movieNumber not to be confused with movieID, which is for the SWCG database (;
+    websiteURL = f'/movies/{movieNumber}'
+    print(websiteURL)
+    if request.method == "POST": #for logging into an account, once form is sent
+        if login:
+            reviewText = request.form.get('reviewText')
+            rating = request.form.get('rating')
+
+
+            con = sqlite3.connect('SWCG.db')
+            cur = con.cursor()
+
+            currentReviews = cur.execute('SELECT review, rating, user FROM reviews WHERE movieID=?', [movieNumber + 1]).fetchall()
+
+
+        
+            #quick pseudocode
+            # if reviewtext doesn't already exist in the table, (with for loop and bool)
+            # add review text into sql review table with name
+            exists = False
+            print(movieNumber + 1)
+            print(reviewText)
+            print(rating)
+            print(sessionUsername)
+
+            print(currentReviews)
+            for i in range(0, len(currentReviews)):
+                checkExistingReview = currentReviews[i][0]
+                checkExistingRating = currentReviews[i][1]
+                checkExistingUser = currentReviews[i][2]
+                print(checkExistingReview)
+                print(checkExistingRating)
+                print(checkExistingUser)
+                if checkExistingReview == reviewText and str(checkExistingRating) == str(rating) and sessionUsername == checkExistingUser: #if name and password runs, then account exists
+                    exists = True
+                    print('real')
+            if exists == True: #check if exists in database
+                print('nice')
+                cur.close()
+                con.close()
+            else: #if not, then adds it to the database
+                cur.execute(''' INSERT INTO reviews VALUES
+                            (?, ?, ?, ?) ''', [reviewText, movieNumber+1, sessionUsername, rating])
+                print('goo')
+                con.commit()
+
+                cur.close()
+                con.close()
+                return redirect('/')
+        
+    
     for i in range(0, len(movieID)): #stops indexing at j-1 because python is dumb!!!
         print(i)
         if movieNumber == movieID[i][0] - 1: #to begin indexing from 0
-            return render_template('review.html', movieNumber=movieNumber, movie=movieRecord, movieReview=movieReview, login=login, sessionUsername=sessionUsername)
+            return render_template('review.html', movieNumber=movieNumber, movie=movieRecord, movieReview=movieReview, login=login, sessionUsername=sessionUsername, websiteURL=websiteURL)
         else:
             next
     return 'Error! Movie not found!' #replace with error page
